b8480bf6c7c5a6d2370d9aa4591ebf4c72c77dce
Clarence.C <tong901@163.com>
2020-10-25 23:40:41 +0800

    为toy-react添加生命周期

--- toy-react.js
+++ toy-react.js
@@ -5,10 +5,14 @@ class ElementWrapper {
     this.root = document.createElement(type)
   }
   setAttribute(name, value) {
-    if (name.match(/^on([\s\S]+)/)) {
+    if (name.match(/^on([\s\S]+)$/)) {
       this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
     } else {
-      this.root.setAttribute(name, value)
+      if (name === 'className') {
+        this.root.setAttribute('class', value)
+      } else {
+        this.root.setAttribute(name, value)
+      }
     }
   }
   appendChild(component) {
@@ -50,14 +54,38 @@ export class Component {
     this.render()[RENDER_TO_DOM](range)
   }
   rerender() {
-    this._range.deleteContents()
-    this[RENDER_TO_DOM](this._range)
+    let oldRange = this._range
+
+    let range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)
+    this[RENDER_TO_DOM](range)
+
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
+  }
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state  = newState
+      this.rerender()
+      return 
+    }
+    let merge = function(oldState, newState) {
+      for (let p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p]
+        } else {
+          merge(oldState[p], newState[p])
+        }
+      }
+    }
+    merge(this.state, newState)
+    this.rerender()
   }
 }
 
 export function createElement(type, attributes, ...children) {
   let e
-  console.log('createElement', type, attributes)
   if (typeof type === 'string') {
     e = new ElementWrapper(type)
   } else {
@@ -71,6 +99,9 @@ export function createElement(type, attributes, ...children) {
     for (let child of children) {
       if (typeof child === "string") {
         child = new TextWrapper(child)
+      }
+      if (child === null) {
+        continue
       }
       if (typeof child === 'object' && (child instanceof Array)) {
         insertChildren(child)
